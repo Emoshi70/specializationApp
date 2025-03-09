de52c3fd61da0f64d879959928b747da60d284a7
Emoshi70 <125546630+Emoshi70@users.noreply.github.com>
2025-03-09 15:00:33 +0800

    Update specialization.py

--- specialization.py
+++ specialization.py
@@ -31,13 +31,13 @@ content = data.load()
 
 text_splitter = RecursiveCharacterTextSplitter(chunk_size=256,chunk_overlap=50)
 chunking = text_splitter.split_documents(content)
-os.environ['HUGGINGFACEHUB_API_TOKEN'] = HF_token
 
 HF_token = st.text_input("Enter Huggingface Token:", type = "password") #getpass()
 clicked = st.button("Submit", key = 1)
 if clicked:
     query = st.text_input("Enter text prompt related to Specializations (Click submit when ready, do not press enter): ")#"What is Bachelor’s Degree in Computer Engineering?"
 if clicked and query:
+    os.environ['HUGGINGFACEHUB_API_TOKEN'] = HF_token
     embeddings = HuggingFaceInferenceAPIEmbeddings(
        api_key = HF_token,model_name = "BAAI/bge-base-en-v1.5"
     )
