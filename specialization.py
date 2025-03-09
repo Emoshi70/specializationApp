37764672b183424b585d4e3471663df0ed797973
Emoshi70 <125546630+Emoshi70@users.noreply.github.com>
2025-03-09 14:56:33 +0800

    Update specialization.py

--- specialization.py
+++ specialization.py
@@ -12,15 +12,16 @@ from langchain.embeddings import HuggingFaceInferenceAPIEmbeddings
 
 import os
 from getpass import getpass
-
+import chromadb.api
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain.vectorstores import Chroma
+from langchain.text_splitter import RecursiveCharacterTextSplitter
 
 # get your free access token from HuggingFace and paste it here
 HF_token = st.text_input("Enter Huggingface Token:", type = "password") #getpass()
 query = st.text_input("Enter text prompt related to Specializations (Click submit when ready, do not press enter): ")#"What is Bachelor’s Degree in Computer Engineering?"
 clicked = st.button("Submit", key = 1)
-if clicked:
-    import chromadb.api
-
+if clicked and query and HF_token:
     chromadb.api.client.SharedSystemClient.clear_system_cache()
     URL = ["https://www.mapua.edu.ph/pages/academics/undergraduate/intramuros-campus/school-of-electrical-electronics-and-computer-engineering/bachelor-of-science-in-computer-engineering",
            "https://www.indeed.com/career-advice/career-development/computer-engineering-specialization",
@@ -33,9 +34,6 @@ if clicked:
     #extract the content
     content = data.load()
     
-    
-    from langchain.text_splitter import RecursiveCharacterTextSplitter
-    
     text_splitter = RecursiveCharacterTextSplitter(chunk_size=256,chunk_overlap=50)
     chunking = text_splitter.split_documents(content)
     os.environ['HUGGINGFACEHUB_API_TOKEN'] = HF_token
@@ -44,8 +42,6 @@ if clicked:
        api_key = HF_token,model_name = "BAAI/bge-base-en-v1.5"
     )
 
-
-    from langchain.vectorstores import Chroma
 
     vectorstore = Chroma.from_documents(chunking, embeddings)
     retriever = vectorstore.as_retriever(search_type="mmr",search_kwargs={"k":3})
